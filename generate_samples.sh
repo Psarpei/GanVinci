ea2b6a2d345d73b307aa5e07d656857dc76e6f2a
psarpei <pascal-fischer1206@gmx.de>
2022-08-28 20:22:35 +0200

    add generate bash script

--- generate_samples.sh
+++ generate_samples.sh
@@ -1,19 +1,23 @@
-edit="big_eyes"
+#edit="big_eyes"
 
 stylegan2_ckpt=checkpoint/stylegan2-ffhq-config-f.pt
 
 truncation=0.8
 sample=1
 pics=20
-alpha=0.1
+alpha=0.25
 seed=42
-att_start=0
-att_layer=8
+att_start=7
+att_layer=18
+att_channel=16
 
-for mask_thresh in 0 0.8
+for edit in blond_hair brown_skin eyeliner eye_shadow green_eyes long_hair makeup pink_lipstick purple_hair red_beard white_teeth  
 do
-  for train_iter in 01000 02000 05000 10000 20000
+  for mask_thresh in 0 0.8
   do
-    python generate.py --ckpt $stylegan2_ckpt --truncation $truncation --sample $sample --pics $pics --alpha $alpha --seed $seed --mask_threshold $mask_thresh --train_iter $train_iter --clip_text $edit --att_layer $att_layer --att_start att_start
+    for train_iter in 01000 02000 05000 #10000 20000
+    do
+      python generate.py --ckpt $stylegan2_ckpt --truncation $truncation --sample $sample --pics $pics --alpha $alpha --seed $seed --mask_threshold $mask_thresh --train_iter $train_iter --clip_text $edit --att_layer $att_layer --att_start $att_start --att_channel $att_channel
+    done
   done
 done
