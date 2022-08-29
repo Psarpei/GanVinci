03796d787b21a0743394d885cdaf249f0fbfcb71
psarpei <pascal-fischer1206@gmx.de>
2022-08-29 17:09:02 +0200

    add only male option

--- train_FEAT.py
+++ train_FEAT.py
@@ -25,6 +25,7 @@ class CLIPLoss(torch.nn.Module):
         cos_distance = 1 - self.model(image, text)[0] / 100
         return cos_distance.mean()
 
+
 def clip_loss(image, text_tokens, clip_model, cos_sim):
     
     image_features = clip_model.encode_image(image)
@@ -34,6 +35,11 @@ def clip_loss(image, text_tokens, clip_model, cos_sim):
     
     return cos_dist.mean()
 
+def clip_most_likely_cat(image, cat_tokens, clip_model):
+
+    logits_per_image, _ = clip_model(image, cat_tokens)
+    probs = logits_per_image.softmax(dim=-1)
+    return probs[0]
 
 def att_reg_loss(mask):
     att_loss = mask.squeeze(1).sum(axis=[-1,-2]) * (1/(mask.size(-1)*mask.size(-2)))
@@ -64,7 +70,7 @@ def total_variation_loss(mask):
     return tv_loss.mean()
 
 
-def train(args, feat, device, mean_latent, optimizer, scheduler, clip_model, text_tokens, cos_sim, clip_loss_s, mse):
+def train(args, feat, device, mean_latent, optimizer, scheduler, clip_model, text_tokens, cos_sim, clip_loss_s, mse, mf_tokens):
     log_string = "iteration: {:5}, loss: {:2.6} l_clip: {:2.6f}, l_att: {:2.6f}, l_l2: {:2.6f}, l_tv: {:2.6f}"
 
     clip_losses = []
@@ -79,7 +85,7 @@ def train(args, feat, device, mean_latent, optimizer, scheduler, clip_model, tex
         
         sample_z = torch.randn(args.batch_size, args.latent, device=device)
 
-        _ , edit_sample, mask, latent, latent_feat = feat(
+        sample, edit_sample, mask, latent, latent_feat = feat(
             [sample_z],
             truncation=args.truncation,
             truncation_latent=mean_latent,
@@ -87,10 +93,14 @@ def train(args, feat, device, mean_latent, optimizer, scheduler, clip_model, tex
             alpha=args.alpha
         )
 
-        edit_resized = F.interpolate(edit_sample,
+        sample_resized = F.interpolate(sample,
                                      size=(224, 224),
                                      mode='bilinear')
 
+        probs = clip_most_likely_cat(sample_resized, mf_tokens, clip_model)
+        
+        if(args.male_only and probs[0] < 0.8):
+            continue
         
         #l_clip = clip_loss(edit_resized, text_tokens, clip_model, cos_sim)
         l_clip = clip_loss_s(edit_sample, text_tokens)
@@ -131,7 +141,7 @@ def train(args, feat, device, mean_latent, optimizer, scheduler, clip_model, tex
         tv_losses.append(l_tv.item())
         losses.append(loss.item())
 
-        if(i in [10, 1000, 2000, 5000, 10000, 20000]):
+        if(i in [1000, 2000, 5000, 10000, 20000]):
             torch.save(
                         {
                             "weights": feat.state_dict(),
@@ -244,11 +254,18 @@ if __name__ == "__main__":
         type=float,
         default=0.5
     )
+    parser.add_argument(
+        "--male_only",
+        action="store_true"
+    )  
+    parser.set_defaults(male_only=False)
 
     args = parser.parse_args()
 
     args.latent = 512
     args.n_mlp = 8
+
+    assert not args.male_only or args.batch_size == 1, "male_only is only possible at batch_size 1"
 
     #initialize generator
     g_ema = Generator(
@@ -264,6 +281,7 @@ if __name__ == "__main__":
     #initialize clip model prerequisites
     clip_model, _ = clip.load("ViT-B/32", device=device)
     text_tokens = clip.tokenize([args.clip_text]).to(device)
+    mf_tokens = clip.tokenize(['male', 'female']).to(device)
     cos_sim = CosineSimilarity()
 
     #calculate mean if using truncation
@@ -297,4 +315,4 @@ if __name__ == "__main__":
     if not os.path.exists(f"edits/{args.att_start}-{args.att_layer}/{args.clip_text}/samples"):
         os.makedirs(f"edits/{args.att_start}-{args.att_layer}/{args.clip_text}/samples")
 
-    train(args, feat, device, mean_latent, optimizer, scheduler, clip_model, text_tokens, cos_sim, clip_loss_s, mse)
+    train(args, feat, device, mean_latent, optimizer, scheduler, clip_model, text_tokens, cos_sim, clip_loss_s, mse, mf_tokens)
