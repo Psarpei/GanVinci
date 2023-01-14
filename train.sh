e9f128d854ad3715e24bb4b4bebad588e7f0d1ec
psarpei <pascal-fischer1206@gmx.de>
2023-01-14 16:36:57 +0100

    remove non necessary things

--- train.sh
+++
@@ -1,9 +0,0 @@
-python train_FEAT.py --batch_size 2 --stylegan2_ckpt checkpoint/stylegan2-ffhq-config-f.pt --truncation 0.9 --clip_text 'eye shadow' --att_layer 18
-python train_FEAT.py --batch_size 2 --stylegan2_ckpt checkpoint/stylegan2-ffhq-config-f.pt --truncation 0.9 --clip_text 'red lipstick' --att_layer 18
-python train_FEAT.py --batch_size 2 --stylegan2_ckpt checkpoint/stylegan2-ffhq-config-f.pt --truncation 0.9 --clip_text 'closed smiling mouth'
-python train_FEAT.py --batch_size 2 --stylegan2_ckpt checkpoint/stylegan2-ffhq-config-f.pt --truncation 0.9 --clip_text 'purple hair' --att_layer 18
-python train_FEAT.py --batch_size 2 --stylegan2_ckpt checkpoint/stylegan2-ffhq-config-f.pt --truncation 0.9 --clip_text 'closed mouth'
-python train_FEAT.py --batch_size 2 --stylegan2_ckpt checkpoint/stylegan2-ffhq-config-f.pt --truncation 0.9 --clip_text 'curly hair'
-python train_FEAT.py --batch_size 2 --stylegan2_ckpt checkpoint/stylegan2-ffhq-config-f.pt --truncation 0.9 --clip_text 'makeup' --att_layer 18
-python train_FEAT.py --batch_size 2 --stylegan2_ckpt checkpoint/stylegan2-ffhq-config-f.pt --truncation 0.9 --clip_text 'mustache'
-
